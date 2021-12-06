d85bba1203297d6af90f7e14e877e2ba4a3cd2dc
wlu04 <wlu04@users.noreply.github.com>
2021-12-06 11:44:50 -0500

    adding more sample_qc steps

--- scripts/sample_qc.py
+++ scripts/sample_qc.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 from typing import Optional, Union
 import hail as hl
+import pickle
 
 from gnomad.utils.filtering import filter_to_autosomes
 from gnomad.utils.annotations import bi_allelic_expr
@@ -18,9 +19,16 @@ from gnomad.sample_qc.relatedness import (
     SIBLINGS,
     UNRELATED,
 )
-from gnomad.sample_qc.ancestry import pc_project
+from gnomad.sample_qc.ancestry import pc_project, assign_population_pcs
 
 from ccdg_qc.resources import *
+
+path_to_gnomad_rf = (
+    "gs://gnomad-public-requester-pays/release/3.1/pca/gnomad.v3.1.RF_fit.pkl"
+)
+path_to_gnomad_loadings = (
+    "gs://gnomad-public-requester-pays/release/3.1/pca/gnomad.v3.1.pca_loadings.ht"
+)
 
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
@@ -34,6 +42,7 @@ def get_qc_vds(
     split: bool = False,
     autosome_only: bool = False,
     interval_qc: bool = False,
+    pca_samples_exome_interval: float = 0.8,
 ) -> hl.vds.VariantDataset:
     """
     Wrapper function to get ccdg vds with desired filtering
@@ -47,8 +56,9 @@ def get_qc_vds(
     if data_type == "exomes" and interval_qc:
         logger.info("Filtering CCDG exomes VDS to high quality intervals...")
         int_ht = hl.read_table(
-            get_ccdg_results_path(data_type=data_type, result="high_qual_intervals")
+            get_ccdg_results_path(data_type=data_type, result="intervals")
         )
+        int_ht = int_ht.filter(int_ht["pct_broad_defined"] > pca_samples_exome_interval)
         vds = hl.vds.filter_intervals(
             vds, intervals=int_ht.interval.collect(), keep=True
         )
@@ -78,6 +88,7 @@ def compute_sample_qc(data_type: str = "genomes") -> hl.Table:
         autosome_only=True,
         split=True,
     )
+    # Use modified compute_stratified_sample_qc with the vds option
     sample_qc_ht = compute_stratified_sample_qc(
         vds,
         strata={
@@ -157,15 +168,12 @@ def main(args):
     data_type = "exomes" if args.exomes else "genomes"
     hl.init(log=f"/ccdg_sample_qc_{data_type}.log")
     # gcloud compute scp wlu-m:/hard_filter_genomes.log .
-    if args.update_interval_qc_ht:
-        int_ht = exomes_qc_intervals_ht(overwrite=args.overwrite)
-
     if args.sample_qc:
         compute_sample_qc(data_type).write(
             get_ccdg_results_path(data_type=data_type, result="sample_qc_all"),
             overwrite=args.overwrite,
         )
-    # Modify when hl.vds.impute_sex() is available
+    ##### Modify when hl.vds.impute_sex() is available
     # if args.impute_sex:
     #     compute_sex().write(
     #         get_ccdg_results_path(data_type=data_type, result="sex"),
@@ -179,6 +187,11 @@ def main(args):
     #     ).write(
     #         get_ccdg_results_path(data_type=data_type, result="sex"),
     #         overwrite=args.overwrite,
+    #     )
+    ##### Wait for more information
+    # if args.compute_hard_filters:
+    #     compute_hard_filters(args.min_cov).write(
+    #         hard_filtered_samples.path, overwrite=args.overwrite
     #     )
 
     if args.run_pc_relate or args.reannotate_relatedness:
@@ -232,27 +245,175 @@ def main(args):
         mt = hl.read_matrix_table(
             get_pca_variants_path(ld_pruned=True, data=f"ccdg_{data_type}", mt=True)
         )
-        related_ht = hl.read_table(
-            get_ccdg_results_path(data_type=data_type, result="related_samples")
-        )
 
-        related_mt = mt.filter_cols(hl.is_defined(related_mt[mt.col_key]), keep=True)
-        pca_mt = mt.filter_cols(hl.is_defined(related_mt[mt.col_key]), keep=False)
-        pca_loadings = hl.read_table(
-            "gs://gnomad-public-requester-pays/release/3.1/pca/gnomad.v3.1.pca_loadings.ht"
-        )
+        pca_loadings = hl.read_table(path_to_gnomad_loadings)
 
         pca_ht = hl.experimental.pc_project(
-            pca_mt, pca_loadings.loadings, pca_loadings.pca_af
+            mt.GT,
+            pca_loadings.loadings,
+            pca_loadings.pca_af,
         )
-        pca_mt = pca_mt.annotate_cols(scores=pca_ht[pca_mt.col_key].scores)
 
-        related_ht = hl.experimental.pc_project(
-            related_mt, pca_loadings.loadings, pca_loadings.pca_af
+        pca_ht.checkpoint(
+            get_ccdg_results_path(
+                data_type=data_type, result="gnomad_pc_project_scores"
+            ),
+            overwrite=args.overwrite,
         )
-        related_mt = related_mt.annotate_cols(
-            scores=related_ht[related_mt.col_key].scores
+
+        # related_ht = hl.read_table(
+        #     get_ccdg_results_path(data_type=data_type, result="related_samples")
+        # )
+        #
+        # related_mt = mt.filter_cols(hl.is_defined(related_mt[mt.col_key]), keep=True)
+        # pca_mt = mt.filter_cols(hl.is_defined(related_mt[mt.col_key]), keep=False)
+
+        # pca_ht = hl.experimental.pc_project(
+        #     pca_mt.GT, pca_loadings.loadings, pca_loadings.pca_af
+        # )
+        # pca_mt = pca_mt.annotate_cols(scores=pca_ht[pca_mt.col_key].scores)
+        #
+        # related_ht = hl.experimental.pc_project(
+        #     related_mt.GT, pca_loadings.loadings, pca_loadings.pca_af
+        # )
+        # related_mt = related_mt.annotate_cols(
+        #     scores=related_ht[related_mt.col_key].scores
+        # )
+
+    if args.assign_pops:
+        with hl.hadoop_open(
+            path_to_gnomad_rf,
+            "rb",
+        ) as f:
+            fit = pickle.load(f)
+
+        # Reduce the scores to only those used in the RF model, this was 6 for v2 and 16 for v3.1
+        n_pcs = fit.n_features_
+        pca_ht = hl.read_table(
+            get_ccdg_results_path(
+                data_type=data_type, result="gnomad_pc_project_scores"
+            )
         )
+        pca_ht = pca_ht.annotate(scores=pca_ht.scores[:n_pcs])
+        pop_ht, rf_model = assign_population_pcs(
+            pca_ht,
+            pc_cols=pca_ht.scores,
+            fit=fit,
+        )
+
+        pop_ht.checkpoint(
+            get_ccdg_results_path(data_type=data_type, result="pop_assignment"),
+            overwrite=args.overwrite,
+            _read_if_exists=not args.overwrite,
+        )
+        pop_ht.transmute(
+            **{f"PC{i + 1}": pop_ht.pca_scores[i] for i in range(n_pcs)}
+        ).export(
+            get_ccdg_results_path(data_type=data_type, result="pop_assignment")[:-2] + "tsv"
+        )
+
+        with hl.hadoop_open(
+            get_ccdg_results_path(data_type=data_type, result="pop_RF_fit")[:-2] + "pickle",
+            "wb",
+        ) as out:
+            pickle.dump(rf_model, out)
+
+    if args.calculate_inbreeding:
+        qc_mt = hl.read_matrix_table(
+            get_pca_variants_path(ld_pruned=True, data=f"ccdg_{data_type}", mt=True)
+        )
+        pop_ht = hl.read_table(
+            get_ccdg_results_path(data_type=data_type, result="pop_assignment"),
+        )
+        qc_mt = qc_mt.annotate_cols(pop=pop_ht[qc_mt.col_key].pop)
+        qc_mt = qc_mt.annotate_rows(
+            call_stats_by_pop=hl.agg.group_by(
+                qc_mt.pop, hl.agg.call_stats(qc_mt.GT, qc_mt.alleles)
+            )
+        )
+        inbreeding_ht = (
+            qc_mt.annotate_cols(
+                inbreeding=hl.agg.inbreeding(
+                    qc_mt.GT, qc_mt.call_stats_by_pop[qc_mt.pop].AF[1]
+                )
+            )
+            .cols()
+            .select("inbreeding")
+        )
+        inbreeding_ht.write(
+            get_ccdg_results_path(data_type=data_type, result="inbreeding"),
+            overwrite=args.overwrite,
+        )
+
+    if args.apply_stratified_filters or args.apply_regressed_filters:
+        filtering_qc_metrics = args.filtering_qc_metrics.split(",")
+        sample_qc_ht = hl.read_table(
+            get_ccdg_results_path(data_type=data_type, result="sample_qc_bi_allelic")
+        )
+        pc_scores = hl.read_table(
+            get_ccdg_results_path(data_type=data_type, result="pc_scores")
+        )
+        sample_qc_ht = sample_qc_ht.select(
+            pc_scores[sample_qc_ht.key],
+        )
+        pop_ht = hl.read_table(
+            get_ccdg_results_path(data_type=data_type, result="pop_assignment"),
+        )
+
+        if "inbreeding" in filtering_qc_metrics:
+            inbreeding_ht = hl.read_table(
+                get_ccdg_results_path(data_type=data_type, result="inbreeding")
+            )[sample_qc_ht.key]
+            sample_qc_ht = sample_qc_ht.annotate(
+                inbreeding=inbreeding_ht.inbreeding.f_stat
+            )
+
+        if args.apply_regressed_filters:
+            n_pcs = args.regress_n_pcs
+            residuals_ht = compute_qc_metrics_residuals(
+                ht=sample_qc_ht,
+                pc_scores=sample_qc_ht.scores[:n_pcs],
+                qc_metrics={
+                    metric: sample_qc_ht[metric] for metric in filtering_qc_metrics
+                },
+            )
+            residuals_ht = residuals_ht.filter(
+                hl.is_missing(hard_filtered_samples.ht()[residuals_ht.key])
+            )
+            stratified_metrics_ht = compute_stratified_metrics_filter(
+                ht=residuals_ht,
+                qc_metrics=dict(residuals_ht.row_value),
+                metric_threshold={
+                    "n_singleton_residual": (math.inf, 8.0),
+                    "r_het_hom_var_residual": (math.inf, 4.0),
+                },
+            )
+
+            residuals_ht = residuals_ht.annotate(
+                **stratified_metrics_ht[residuals_ht.key]
+            )
+            residuals_ht = residuals_ht.annotate_globals(
+                **stratified_metrics_ht.index_globals(),
+                n_pcs=n_pcs,
+            )
+        else:
+            logger.info(
+                "Computing stratified QC metrics filters using metrics: "
+                + ", ".join(filtering_qc_metrics)
+            )
+            sample_qc_ht = sample_qc_ht.annotate(qc_pop=pop_ht[sample_qc_ht.key].pop)
+            # TODO: compute hard-filtered samples
+            sample_qc_ht = sample_qc_ht.filter(
+                hl.is_missing(hard_filtered_samples.ht()[sample_qc_ht.key])
+            )
+            stratified_metrics_ht = compute_stratified_metrics_filter(
+                sample_qc_ht,
+                qc_metrics={
+                    metric: sample_qc_ht[metric] for metric in filtering_qc_metrics
+                },
+                strata={"qc_pop": sample_qc_ht.qc_pop},
+                metric_threshold={"n_singleton": (4.0, 8.0)},
+            )
 
 
 if __name__ == "__main__":
@@ -261,11 +422,6 @@ if __name__ == "__main__":
     parser.add_argument(
         "--exomes",
         help="Sample QC on CCDG exomes. Otherwise, default is CCDG genomes.",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--update_interval_qc_ht",
-        help="Update Hail Table of high quality CCDG exomes intervals",
         action="store_true",
     )
     parser.add_argument(
@@ -279,6 +435,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "--reannotate_sex",
         help="Runs the sex karyotyping annotations again, without re-computing sex imputation metrics.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--compute_hard_filters",
+        help="Computes samples to be hard-filtered",
         action="store_true",
     )
     parser.add_argument("--run_pc_relate", help="Run PC-relate", action="store_true")
@@ -325,6 +486,47 @@ if __name__ == "__main__":
         "--run_pc_project",
         help="Project CCDG data onto gnomAD loadings",
         action="store_true",
+    )
+    parser.add_argument(
+        "--assign_pops", help="Assigns pops from PCA", action="store_true"
+    )
+    parser.add_argument(
+        "--calculate_inbreeding",
+        help="Calculate sample level inbreeding",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--filtering_qc_metrics",
+        help="List of QC metrics for filtering.",
+        default=",".join(
+            [
+                "n_snp",
+                "n_singleton",
+                "r_ti_tv",
+                "r_insertion_deletion",
+                "n_insertion",
+                "n_deletion",
+                "r_het_hom_var",
+                "n_transition",
+                "n_transversion",
+            ]
+        ),
+    )  # used in v3 'n_het', 'n_hom_var',
+    parser.add_argument(
+        "--apply_stratified_filters",
+        help="Compute per pop filtering.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--apply_regressed_filters",
+        help="Computes qc_metrics adjusted for pop.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--regress_n_pcs",
+        help="Number of PCs to use for qc metric regressions",
+        default=10,
+        type=int,
     )
     args = parser.parse_args()
     main(args)
